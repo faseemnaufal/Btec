788d02ae743410dca3d784c4dd40c5776dd70f37
faseemnaufal <faseemnaufal@gmail.com>
2024-01-27 00:20:03 +0530

    latest commit

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -8,16 +8,40 @@ function HeroSection() {
     <section className="flex justify-between items-center mt-2 pl-1 pr-1 pb-1 md:z-[9999] rounded-lg"
     style={{ backgroundImage: "url('/images/booksidee.jpg')" }} 
     >
-      <div className="pt-32 md:pt-4 md:flex-row items-center md:justify-between mt-16 w-full">
+      <div className="pt-32 md:pt-4 md:flex-row items-center md:justify-between mt-16 w-full"
+      >
         <p className="text-[1.128rem] font-[700] text-slate-900 uppercase mb-4">
         Empowering future generation through the Education.
         </p>
         <div className="flex flex-col">
           
-
-          <p className="volkhov font-[700] text-[1.7rem] sm:text-[2rem] md:text-[4.43756rem] leading-large inline-flex bg-blue-800 text-white">
+        <img
+          src="/images/b.jpg"
+          alt="logo group"
+          // style={{ width: '100%', height: 'auto',  transition: 'opacity 0.5s ease-in-out', }}
+          className="animated-image"
+        />
+        <img
+          src="/images/t.jpg"
+          alt="logo group"
+          // style={{ width: '100%', height: 'auto',  transition: 'opacity 0.5s ease-in-out', }}
+          className="animated-image"
+        />
+        <img
+          src="/images/e.jpg"
+          alt="logo group"
+          // style={{ width: '100%', height: 'auto',  transition: 'opacity 0.5s ease-in-out', }}
+          
+        />
+        <img
+          src="/images/c.jpg"
+          alt="logo group"
+          // style={{ width: '100%', height: 'auto' }}
+          className="animated-image"
+        />
+          {/* <p className="volkhov font-[700] text-[1.7rem] sm:text-[2rem] md:text-[4.43756rem] leading-large inline-flex bg-blue-800 text-white">
           BUSINESS & TECHNOLOGICAL EDUCATION COLLEGE
-          </p>
+          </p> */}
         </div>
 
         <p className="my-[1.6rem] font-bold leading-[1.692rem] text-lightGray text-[1rem]">
